ba7ba4352055b0baf5b38a25952ea8be3e64776a
Daniel Aguiar <daniel.r.aguiar@economia.gov.br>
2023-12-15 10:29:54 -0300

    changed the golang files

--- math_test.go
+++ math_test.go
@@ -2,11 +2,11 @@ package main
 
 import "testing"
 
-func TestSoma ( t *testing.T){
+func TestSoma(t *testing.T) {
 
-	total: Soma(15, 15)/
-	
+	total := soma(15, 15)
+
 	if total != 30 {
-		t.Errorf("resultado da soma é invalido. Resultado %d, Esperado %d", total, 30)
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
